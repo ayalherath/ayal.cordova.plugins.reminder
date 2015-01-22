4c3927ee42cef8f023c802623ae742f431720434
ayalherath <ayalherath@gmail.com>
2015-01-22 16:43:31 +0530

    Update Reminder.js

--- www/Reminder.js
+++ www/Reminder.js
@@ -12,6 +12,23 @@ Reminder.prototype.getAllReminders = function (successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, "Reminder", "getAllReminders");
 };
 
+Reminder.prototype.deleteReminderByTitle = function (title, successCallback, errorCallback) {
+   cordova.exec(successCallback, errorCallback, "Reminder", "deleteReminderByTitle", [{
+    "reminderTitle": title
+     }]);
+};
+               
+Reminder.prototype.deleteReminderBycalendarItemIdentifier = function (calendarItemIdentifier, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Reminder", "deleteReminderByCalendarItemIdentifier", [{
+     "calendarItemIdentifier": calendarItemIdentifier
+     }]);
+};
+               
+Reminder.prototype.markAsCompleated = function (calendarItemIdentifier, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Reminder", "markAsCompleated", [{
+     "calendarItemIdentifier": calendarItemIdentifier
+     }]);
+};
 
 Reminder.install = function () {
   if (!window.plugins) {
