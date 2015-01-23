f0d4332a48269f15805bcc560ba80c6b2a38e363
ayalherath <ayalherath@gmail.com>
2015-01-23 11:31:07 +0530

    Update Reminder.m

--- src/ios/Reminder.m
+++ src/ios/Reminder.m
@@ -44,166 +44,178 @@
 
 -(void)getAllReminders:(CDVInvokedUrlCommand*)command
 {
-    NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
-    [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
-     {
-         NSMutableArray *results = [[NSMutableArray alloc] initWithCapacity:reminders.count];
-         
-         NSDateFormatter *df = [[NSDateFormatter alloc] init];
-         [df setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-         
-         for(int i=0; i<reminders.count; i++)
+    [self.commandDelegate runInBackground:^{
+        NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
+        [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
          {
-             EKReminder *reminder = [reminders objectAtIndex:i];
-
-             NSMutableDictionary *entry = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
-                                           reminder.title, @"title",
-                                           reminder.calendarItemIdentifier, @"calendarItemIdentifier",
-                                           [NSString stringWithFormat:@"%ld", (long)reminder.priority], @"priority",
-                                           nil];
-             if (reminder.completionDate != nil)
-             {
-                 [entry setObject:[df stringFromDate:reminder.completionDate] forKey:@"completionDate"];
-             }
+             NSMutableArray *results = [[NSMutableArray alloc] initWithCapacity:reminders.count];
              
-             if (reminder.creationDate != nil)
-             {
-                 [entry setObject:[df stringFromDate:reminder.creationDate] forKey:@"completionDate"];
-             }
+             NSDateFormatter *df = [[NSDateFormatter alloc] init];
+             [df setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
              
-             if ([NSString stringWithFormat:@"%ld", (long)reminder.completed])
+             for(int i=0; i<reminders.count; i++)
              {
-                 [entry setObject:[NSString stringWithFormat:@"%ld", (long)reminder.completed] forKey:@"completed"];
+                 EKReminder *reminder = [reminders objectAtIndex:i];
+    
+                 NSMutableDictionary *entry = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                                               reminder.title, @"title",
+                                               reminder.calendarItemIdentifier, @"calendarItemIdentifier",
+                                               [NSString stringWithFormat:@"%ld", (long)reminder.priority], @"priority",
+                                               nil];
+                 if (reminder.completionDate != nil)
+                 {
+                     [entry setObject:[df stringFromDate:reminder.completionDate] forKey:@"completionDate"];
+                 }
+                 
+                 if (reminder.creationDate != nil)
+                 {
+                     [entry setObject:[df stringFromDate:reminder.creationDate] forKey:@"completionDate"];
+                 }
+                 
+                 if ([NSString stringWithFormat:@"%ld", (long)reminder.completed])
+                 {
+                     [entry setObject:[NSString stringWithFormat:@"%ld", (long)reminder.completed] forKey:@"completed"];
+                 }
+                 
+                 [results addObject:entry];
              }
-             
-             [results addObject:entry];
-         }
-         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsArray:results];
-         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsArray:results];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+    }];
 }
 
 
 - (void)addReminder:(CDVInvokedUrlCommand*)command
 {
-    NSDictionary* options = [command.arguments objectAtIndex:0];
-    NSString* reminderTitle      = [options objectForKey:@"reminderTitle"];
-    EKReminder *reminder = [EKReminder reminderWithEventStore:eventStore];
-    [reminder setTitle:reminderTitle];
-    EKCalendar *defaultReminderList = [eventStore defaultCalendarForNewReminders];
-    [reminder setCalendar:defaultReminderList];
-    NSError *error = nil;
-    BOOL success = [eventStore saveReminder:reminder
-                                     commit:YES
-                                      error:&error];
-    if(success)
-    {
-        NSLog(@"Successfully added the reminder");
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
-    else
-    {
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Error adding the reminder"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        return;
-    }
+    [self.commandDelegate runInBackground:^{
+        NSDictionary* options = [command.arguments objectAtIndex:0];
+        NSString* reminderTitle      = [options objectForKey:@"reminderTitle"];
+        EKReminder *reminder = [EKReminder reminderWithEventStore:eventStore];
+        [reminder setTitle:reminderTitle];
+        EKCalendar *defaultReminderList = [eventStore defaultCalendarForNewReminders];
+        [reminder setCalendar:defaultReminderList];
+        NSError *error = nil;
+        BOOL success = [eventStore saveReminder:reminder
+                                         commit:YES
+                                          error:&error];
+        if(success)
+        {
+            NSLog(@"Successfully added the reminder");
+            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+        else
+        {
+            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Error adding the reminder"];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            return;
+        }
+    }];
 }
 
 -(void)deleteReminderByTitle:(CDVInvokedUrlCommand*)command
 {
-    NSDictionary* options = [command.arguments objectAtIndex:0];
-    NSString* reminderTitle = [options objectForKey:@"reminderTitle"];
-    
-    NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
-    [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
-     {
-         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"title matches %@", reminderTitle];
-         NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
-         
-         for(int i=0;i<results.count;i++)
+    [self.commandDelegate runInBackground:^{
+        NSDictionary* options = [command.arguments objectAtIndex:0];
+        NSString* reminderTitle = [options objectForKey:@"reminderTitle"];
+        
+        NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
+        [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
          {
-             NSError *error = nil;
-             [self.eventStore removeReminder:[results objectAtIndex:i] commit:YES error:&error];
-         }
-         
-         NSLog(@"Successfully deleted the reminders");
-         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+             NSPredicate *predicate = [NSPredicate predicateWithFormat:@"title matches %@", reminderTitle];
+             NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
+             
+             for(int i=0;i<results.count;i++)
+             {
+                 NSError *error = nil;
+                 [self.eventStore removeReminder:[results objectAtIndex:i] commit:YES error:&error];
+             }
+             
+             NSLog(@"Successfully deleted the reminders");
+             CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+    }];
 }
 
 -(void)deleteReminderByCalendarItemIdentifier:(CDVInvokedUrlCommand*)command
 {
-    NSDictionary* options = [command.arguments objectAtIndex:0];
-    NSString* reminderTitle = [options objectForKey:@"calendarItemIdentifier"];
-    
-    NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
-    [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
-     {
-         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"calendarItemIdentifier matches %@", reminderTitle];
-         NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
-         
-         for(int i=0;i<results.count;i++)
+    [self.commandDelegate runInBackground:^{
+        NSDictionary* options = [command.arguments objectAtIndex:0];
+        NSString* reminderTitle = [options objectForKey:@"calendarItemIdentifier"];
+        
+        NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
+        [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
          {
-             NSError *error = nil;
-             [self.eventStore removeReminder:[results objectAtIndex:i] commit:YES error:&error];
-         }
-         
-         NSLog(@"Successfully deleted the reminder");
-         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+             NSPredicate *predicate = [NSPredicate predicateWithFormat:@"calendarItemIdentifier matches %@", reminderTitle];
+             NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
+             
+             for(int i=0;i<results.count;i++)
+             {
+                 NSError *error = nil;
+                 [self.eventStore removeReminder:[results objectAtIndex:i] commit:YES error:&error];
+             }
+             
+             NSLog(@"Successfully deleted the reminder");
+             CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+    }];
 }
 
 -(void) markAsCompleated:(CDVInvokedUrlCommand*)command
 {
-    NSDictionary* options = [command.arguments objectAtIndex:0];
-    NSString* calendarItemIdentifier = [options objectForKey:@"calendarItemIdentifier"];
-    
-    NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
-    [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
-     {
-         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"calendarItemIdentifier matches %@", calendarItemIdentifier];
-         NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
-         
-         for(int i=0;i<results.count;i++)
+    [self.commandDelegate runInBackground:^{
+        NSDictionary* options = [command.arguments objectAtIndex:0];
+        NSString* calendarItemIdentifier = [options objectForKey:@"calendarItemIdentifier"];
+        
+        NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
+        [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
          {
-             EKReminder *reminder = [results objectAtIndex:i];
-             reminder.completed = 1;
-             NSError *error = nil;
-             [self.eventStore saveReminder:reminder commit:YES error:&error];
-         }
-         
-         NSLog(@"Successfully Compleated the reminder");
-         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+             NSPredicate *predicate = [NSPredicate predicateWithFormat:@"calendarItemIdentifier matches %@", calendarItemIdentifier];
+             NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
+             
+             for(int i=0;i<results.count;i++)
+             {
+                 EKReminder *reminder = [results objectAtIndex:i];
+                 reminder.completed = 1;
+                 NSError *error = nil;
+                 [self.eventStore saveReminder:reminder commit:YES error:&error];
+             }
+             
+             NSLog(@"Successfully Compleated the reminder");
+             CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+    }];
 }
 
 -(void) markAsPending:(CDVInvokedUrlCommand*)command
 {
-    NSDictionary* options = [command.arguments objectAtIndex:0];
-    NSString* calendarItemIdentifier = [options objectForKey:@"calendarItemIdentifier"];
-    
-    NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
-    [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
-     {
-         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"calendarItemIdentifier matches %@", calendarItemIdentifier];
-         NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
-         
-         for(int i=0;i<results.count;i++)
+    [self.commandDelegate runInBackground:^{
+        NSDictionary* options = [command.arguments objectAtIndex:0];
+        NSString* calendarItemIdentifier = [options objectForKey:@"calendarItemIdentifier"];
+        
+        NSPredicate *predicate = [eventStore predicateForRemindersInCalendars:nil];
+        [eventStore fetchRemindersMatchingPredicate:predicate completion:^(NSArray *reminders)
          {
-             EKReminder *reminder = [results objectAtIndex:i];
-             reminder.completed = 0;
-             NSError *error = nil;
-             [self.eventStore saveReminder:reminder commit:YES error:&error];
-         }
-         
-         NSLog(@"Successfully Compleated the reminder");
-         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+             NSPredicate *predicate = [NSPredicate predicateWithFormat:@"calendarItemIdentifier matches %@", calendarItemIdentifier];
+             NSArray *results = [reminders filteredArrayUsingPredicate:predicate];
+             
+             for(int i=0;i<results.count;i++)
+             {
+                 EKReminder *reminder = [results objectAtIndex:i];
+                 reminder.completed = 0;
+                 NSError *error = nil;
+                 [self.eventStore saveReminder:reminder commit:YES error:&error];
+             }
+             
+             NSLog(@"Successfully Compleated the reminder");
+             CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+    }];
 }
 
 @end
